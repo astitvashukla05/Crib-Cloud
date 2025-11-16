a74ac308942baa4ba9c636d0c20b052958828202
Astitva Shukla <astitvashukla1605@gmail.com>
2025-11-16 23:41:59 +0530

    feat: add FeatureSection component to landing page

--- src/app/(nondashboard)/landing/page.tsx
+++ src/app/(nondashboard)/landing/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import HeroSection from './HeroSection';
+import FeatureSection from './FeatureSection';
 
 const Landing = () => {
   return (
     <div>
       <HeroSection />
+      <FeatureSection />
     </div>
   );
 };
