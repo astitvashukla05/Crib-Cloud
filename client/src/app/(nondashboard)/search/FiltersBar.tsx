ac651e5d105856fc22a0e472a44d03f88d3b0dbf
Astitva Shukla <astitvashukla1605@gmail.com>
2025-12-02 09:40:46 +0530

    feat: Add property details, location, and application functionality with UI components

--- client/src/app/(nondashboard)/search/FiltersBar.tsx
+++ client/src/app/(nondashboard)/search/FiltersBar.tsx
@@ -78,7 +78,7 @@ const FiltersBar = () => {
         `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
           searchInput
         )}.json?access_token=${
-          process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
+          process.env.NEXT_PUBLIC_MAPBOX_API_KEY
         }&fuzzyMatch=true`
       );
       const data = await response.json();
